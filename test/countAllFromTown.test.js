23600e193335ddfef82021fa63f11599800c7982
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-08-03 11:59:53 +0200

    made changes for testing

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 var countAllFromTown = require('../countAllFromTown.js');
 describe('countAllFromTown', function () {
   it("should return '[ 'CL 124', 'CL 345', 'CL 341' ] []' when given 'regList[i].startsWith(location)'", function () {
+      var fromStellies = countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL');
       assert.deepEqual(fromStellies, [ 'CL 124', 'CL 345', 'CL 341' ]);
   });
 });
