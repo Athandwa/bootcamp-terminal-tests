1e1a3d7247ab7754dd6555db0657945d8bde37f0
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-08-03 11:57:01 +0200

    removed the tests

--- countAllFromTown.js
+++ countAllFromTown.js
@@ -10,11 +10,11 @@ function allFromTown(regNo,location) {
     }
       return fromTown;
 }
-var fromStellies = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL');
-console.log(fromStellies);
-assert.deepEqual(fromStellies, ['CL 124', 'CL 345', 'CL 341']);
-var fromKuilsriver = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CF');
-console.log(fromKuilsriver);
-assert.deepEqual(fromKuilsriver, []);
+// var fromStellies = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL');
+// console.log(fromStellies);
+// assert.deepEqual(fromStellies, ['CL 124', 'CL 345', 'CL 341']);
+// var fromKuilsriver = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CF');
+// console.log(fromKuilsriver);
+// assert.deepEqual(fromKuilsriver, []);
 
 module.exports = allFromTown;
